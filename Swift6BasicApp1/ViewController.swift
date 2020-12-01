52e43d514bce29cfa154a621963fc4e7a51fd6af
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-01 23:21:54 +0900

    パーツつなぎこみ

--- Swift6BasicApp1/ViewController.swift
+++ Swift6BasicApp1/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var imageView: UIImageView!
+  
+  @IBOutlet weak var blurView: UIVisualEffectView!
+  
+  @IBOutlet weak var countLabel: UILabel!
+  
+  var count = 0
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
-
+  @IBAction func tap(_ sender: Any) {
+    
+  }
+  
 }
 
